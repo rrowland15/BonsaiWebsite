9f50e499b04a3b6ed94495a27454cb663846cfd6
Ryan Rowland <rcrowland15@gmail.com>
2024-10-27 01:52:26 -0700

    same as previous commit

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 import logging
+import sqlite3
+
+from gcstorage_interaction import delete_blob, upload_blob
+
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -14,13 +18,104 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    connection = sqlite3.connect("sqlite/mytest.db")
+    cursor = connection.cursor()
+    query = 'SELECT imagelink, albumtitle, imagedescription from UserTreePictures Where email = "album@gmail.com" and albumcover = True'
+    albumdata = cursor.execute(query).fetchall()
     if request.method == 'POST':
         pass
-    return render_template("index.html")
+
+    
+    return render_template("index.j2", albumdata=albumdata)
+
+@app.route("/deletealbum/<albumtitle>", methods=['GET', 'POST'])
+def deletealbum(albumtitle):
+    albumtitle = '"' + albumtitle + '"'
+    
+    connection = sqlite3.connect("sqlite/mytest.db")
+    cursor = connection.cursor()
+    print(albumtitle)
+
+    deleteImagesQuery = '''SELECT imagelink from UserTreePictures where albumtitle = {}'''.format(albumtitle)
+    print(deleteImagesQuery)
+
+    imagelinks = cursor.execute(deleteImagesQuery).fetchall()
+    print(imagelinks)
+
+    deleteDatabaseEntries = '''DELETE FROM UserTreePictures Where albumtitle = {}'''.format(albumtitle)
+    deleteEntries = cursor.execute(deleteDatabaseEntries)
+
+    for link in imagelinks:
+        dellink = "rcrowland15/"+link[0]
+        print(dellink)
+        delete_blob('treephotos',dellink)
+
+    connection.commit()
+    cursor.close()
+
+    return redirect("/")
+
 
 @app.route("/createalbum", methods=['GET', 'POST'])
 def createalbum():
-     return render_template("createalbum.html")
+    print("here",request.method)
+    if request.method == 'POST':
+        print(request.form)
+        print(request.files)
+
+        #
+        username = "album@gmail.com"    #this should be the same as blob location in final
+        blob_location = "treephotos/rcrowland15"
+
+        # album cover
+        imageupload = request.files['imageUpload']
+        albumname = request.form["albumname"]
+        albumcontents = request.form["albumcontents-input"]
+
+        print(type(imageupload))
+
+        # journal entry
+        imageupload2 = request.files['imageUpload2']
+        journalname = request.form["journal-name-input"]
+        journalcontent = request.form["journal-content-input"]
+
+        
+        # this could be a microservice
+        bucket_name = "treephotos"
+        strippedalbumname = albumname.replace(" ","").lower()
+        print(strippedalbumname)
+        destination_blob_name = "rcrowland15/"+strippedalbumname+".jpg"
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name) # destination blob name can have a folder created by document/newfilename
+        blob.upload_from_file(imageupload)
+
+        print(f"File {imageupload} uploaded to {destination_blob_name}.")
+
+        # add stuff to database
+        connection = sqlite3.connect("sqlite/mytest.db")
+        cursor = connection.cursor()
+        tempname = '"'+strippedalbumname+'.jpg"'
+        tempalbumname = '"'+albumname+'"'
+        tempalbumcontents = '"'+albumcontents+'"'
+        query = '''Insert Into UserTreePictures Values ("album@gmail.com", {}, True, {}, {}, "1/1/2025")'''.format(tempname,tempalbumname,tempalbumcontents)
+        count = cursor.execute(query)
+        connection.commit()
+        print("Executed")
+        cursor.close()
+        
+
+
+
+        
+
+    return render_template("createalbum.html")
+
+
+@app.route("/entrytemplate", methods=['GET', 'POST'])
+def entrytemplate():
+
+    return render_template("entrytemplate.html")
 
 
 @app.route('/upload', methods=['GET','POST'])
@@ -30,42 +125,15 @@ def upload():
 
     treepic = request.form["imagefile"]
 
-    
-
-
+    # this could be a microservice
     bucket_name = "treephotos"
     destination_blob_name = "somenewname"
-
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name) # destination blob name can have a folder created by document/newfilename
-
-    # with open(treepic,"rb") as f:
-    #     file_data = f.read()
     blob.upload_from_string(treepic)
 
     print(f"File {treepic} uploaded to {destination_blob_name}.")
-    #print(response)
-    #root = tk.Tk()
-    #root.withdraw()  # Hide the main window
-
-    # file_path = filedialog.askopenfilename()
-
-    # if file_path:
-    #     print("Selected file:", file_path)
-    # app.logger.info('Hello-world applog')
-    # print("here")
-    # if 'file' not in request.files:
-    #     print('No file part')
-
-    # file = request.files['file']
-    # if file.filename == '':
-    #     print('No selected file')
-
-    # # Process the uploaded file here (e.g., save it to a directory)
-    # # file.save(file.filename)
-    # elif file.filename != '':
-    #     print(file.filename)
 
     return 'File uploaded successfully'
 
--- gcstorage_interaction.py
+++ gcstorage_interaction.py
@@ -40,15 +40,42 @@ def download_blob(bucket_name, source_file_name, destination_blob_name):
     print(f"File {source_file_name} donwloaded from {destination_blob_name}.")
 
 
+def delete_blob(bucket_name, blob_name):
+    """Deletes a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # blob_name = "your-object-name"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    generation_match_precondition = None
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to delete is aborted if the object's
+    # generation number does not match your precondition.
+    blob.reload()  # Fetch blob metadata to use in generation_match_precondition.
+    generation_match_precondition = blob.generation
+
+    blob.delete(if_generation_match=generation_match_precondition)
+
+    print(f"Blob {blob_name} deleted.")
+
+
 #Example usage create_bucket:
 #create_bucket("tree_test")
 
 # Example usage upload_blob
-bucket_name = "treephotos"
-source_file_name = "/Users/ryanrowland/Desktop/gingko.jpg"
-destination_blob_name = "gingkoleaf"
-upload_blob(bucket_name, source_file_name, destination_blob_name)
+# bucket_name = "treephotos"
+# source_file_name = "/Users/ryanrowland/Desktop/gingko.jpg"
+# destination_blob_name = "gingkoleaf"
+# upload_blob(bucket_name, source_file_name, destination_blob_name)
 
 #Example usage of download_blob
 #download_blob('tree_images',os.path.join(os.getcwd(),'file1.jpg'),'little_tree')
 #print(os.getcwd())
+
+#Example usage of delete_blob:
+# bucket_name = "treephotos"
+# blob_name = "rcrowland15/uploadlogo.jpg"
+# delete_blob(bucket_name, blob_name)
